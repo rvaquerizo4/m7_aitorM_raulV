d517b35fc56ad68b82ae9bef2e8d8ec0e067b435
amonge20 <amonge20@jaumebalmes.net>
2022-09-29 20:05:32 +0200

    Parte 2

--- Vehicle.php
+++ Vehicle.php
@@ -5,5 +5,102 @@ class Vehicle{
     private $num_serie;
     private $peso;
     private $combustible;
+
+    /**
+     * @return mixed
+     */
+    public function getCombustible()
+    {
+        return $this->combustible;
+    }
+
+    /**
+     * @param mixed $combustible
+     */
+    public function setCombustible($combustible)
+    {
+        $this->combustible = $combustible;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPeso()
+    {
+        return $this->peso;
+    }
+
+    /**
+     * @param mixed $peso
+     */
+    public function setPeso($peso)
+    {
+        $this->peso = $peso;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    /**
+     * @param mixed $modelo
+     */
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getNumSerie()
+    {
+        return $this->num_serie;
+    }
+
+    /**
+     * @param mixed $num_serie
+     */
+    public function setNumSerie($num_serie)
+    {
+        $this->num_serie = $num_serie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * @param mixed $marca
+     */
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+    }
+
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+    }
+
+    function kmRecorregut(){
+        $km = rand(int 0, int 100): int;
+        return km;
+    }
+
+    function vehicleCreat(marca){
+        $cavalls = rand(int 0, int 100): int;
+        return "vehicle es ", $marca, "i té " $cavalls;
+    }
+
 }
 ?>
