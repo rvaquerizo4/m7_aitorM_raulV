57d97455956fb62ab5bafcd39d65ffed36dafd3f
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2022-10-12 13:15:53 +0200

    parte b hecha

--- practica_producte/index.php
+++ practica_producte/index.php
@@ -1,16 +1,4 @@
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-  <?php
+<?php
     $db_host= "localhost";
     $db_nombre = "products";
     $db_usuario = "root";
@@ -22,28 +10,10 @@
 
     $product = mysqli_query($connexio ,$consulta);
 
+    //Insertar product
+    $Fname =  $_REQUEST['Fname'];
+    $Fdescription = $_REQUEST['Fdescription'];
+    $Fprice =  $_REQUEST['Fprice'];
+
+    $sql = "INSERT INTO products  VALUES ('$Fname','$lFdescription','$Fprice')";
 ?>
-    <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Name</th>
-        <th scope="col">Description</th>
-        <th scope="col">Price</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($products as $i => $product) { ?>
-        <tr> 
-          <th scope="row"><?php echo $i + 1 ?></th>
-          <td><?php echo $product['Name'] ?></td>
-          <td><?php echo $product['Description'] ?></td>
-          <td><?php echo $product['price'] ?></td>
-          <td><button type="button" class="btn btn-outline-primary">Edit</button></td>
-          <td><button type="button" class="btn btn-outline-danger">Delete</button></td>
-        </tr>
-      <?php } ?>
-    </tbody>
-  </table>
-</body>
-</html>
