6134e0dcc3c39e4f93b0c2b1a93af7a1166aeddb
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2022-10-12 12:38:37 +0200

    primera parte acabada

--- practica_producte/index.php
+++ practica_producte/index.php
@@ -1,16 +1,3 @@
-<?php
-$db_host= "localhost";
-$db_nombre = "products";
-$db_usuario = "root";
-$db_passwd = "";
-
-$connexio = mysqli_connect($db_host, $db_usuario, $db_passwd, $db_nombre);
-
-$consulta = "SELECT * FROM PRODUCTS";
-
-$product = mysqli_query($connexio ,$consulta);
-
-<html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -23,18 +10,40 @@ $product = mysqli_query($connexio ,$consulta);
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  <?php
+    $db_host= "localhost";
+    $db_nombre = "products";
+    $db_usuario = "root";
+    $db_passwd = "";
 
-    <!-- Optional JavaScript; choose one of the two! -->
+    $connexio = mysqli_connect($db_host, $db_usuario, $db_passwd, $db_nombre);
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    $consulta = "SELECT * FROM PRODUCTS";
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  </body>
+    $product = mysqli_query($connexio ,$consulta);
+
+?>
+    <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Description</th>
+        <th scope="col">Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($products as $i => $product) { ?>
+        <tr> 
+          <th scope="row"><?php echo $i + 1 ?></th>
+          <td><?php echo $product['Name'] ?></td>
+          <td><?php echo $product['Description'] ?></td>
+          <td><?php echo $product['price'] ?></td>
+          <td><button type="button" class="btn btn-outline-primary">Edit</button></td>
+          <td><button type="button" class="btn btn-outline-danger">Delete</button></td>
+        </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+</body>
 </html>
-
