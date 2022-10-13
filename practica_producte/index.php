2c404003f6f08518f642739d3b56009f3689f7e1
rvaquerizo4 <113448166+rvaquerizo4@users.noreply.github.com>
2022-10-13 16:40:24 +0200

    Update index.php

--- practica_producte/index.php
+++ practica_producte/index.php
@@ -35,6 +35,28 @@
     // Close connection
     mysqli_close($connexio);
 ?>
+ <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Description</th>
+        <th scope="col">Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($products as $i => $product) { ?>
+        <tr> 
+          <th scope="row"><?php echo $i + 1 ?></th>
+          <td><?php echo $product['Name'] ?></td>
+          <td><?php echo $product['Description'] ?></td>
+          <td><?php echo $product['price'] ?></td>
+          <td><button type="button" class="btn btn-outline-primary">Edit</button></td>
+          <td><button type="button" class="btn btn-outline-danger">Delete</button></td>
+        </tr>
+      <?php } ?>
+    </tbody>
+  </table>
 </body>
 </html>
                
